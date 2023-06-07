5ff8510aa1f65bee54f22c22fe5725f8116d611b
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-06 23:10:12 -0300

    feat: endpoint for saving staves

---
+++ src/pautas/pautas.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Body, Res, Post } from '@nestjs/common';
+import { Response } from 'express';
+import { PautasService } from './pautas.service';
+import { CriarPautaResource } from './pautas.resource';
+
+@Controller('pautas')
+export class PautasController {
+  constructor(private readonly service: PautasService) {}
+
+  @Post()
+  save(@Body() pauta: CriarPautaResource, @Res() response: Response) {
+    return response.status(201).send(pauta);
+  }
+}
