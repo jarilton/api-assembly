53cee0dd024b4735fefe021a5392759b71b081c7
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 11:35:01 -0300

    feat: Creating Session Initialization Logic

--- src/pautas/pautas.service.ts
+++ src/pautas/pautas.service.ts
@@ -5,6 +5,8 @@ import { Result } from '../common/result';
 
 @Injectable()
 export class PautasService {
+  static TEMPO_PADRAO_PAUTA = 10;
+
   constructor(
     @Inject('PAUTA_REPOSITORY')
     private readonly pautaRepository: Repository<Pauta>,
@@ -31,5 +33,21 @@ export class PautasService {
 
   async findAll(): Promise<Pauta[]> {
     return await this.pautaRepository.find();
+  }
+
+  async iniciarSessao(
+    pauta: Pauta,
+    minutos: number = PautasService.TEMPO_PADRAO_PAUTA,
+  ): Promise<boolean> {
+    if (!pauta.isPossivelIniciarSessao()) {
+      return false;
+    }
+
+    pauta.abertura = new Date();
+    pauta.fechamento = new Date(pauta.abertura.getTime() + minutos * 60000);
+
+    await this.pautaRepository.update(pauta.id, pauta);
+
+    return true;
   }
 }
