4f808edc2c59e0221fd9581519ad967cf5f2b8d6
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 19:57:26 -0300

    feat: validation using class-validation

--- src/votos/votos.resource.ts
+++ src/votos/votos.resource.ts
@@ -1,6 +1,17 @@
 import { OpcaoVoto } from './votos.entity';
+import { IsNotEmpty, IsIn } from 'class-validator';
 
 export class RegistroVotoResource {
+  @IsNotEmpty({
+    message: 'CPF é obrigatório',
+  })
   cpf: string;
+
+  @IsNotEmpty({
+    message: 'Opção de voto é obrigatória',
+  })
+  @IsIn([OpcaoVoto.SIM, OpcaoVoto.NAO], {
+    message: 'Opção de voto só pode ser SIM ou NAO',
+  })
   opcaoVoto: OpcaoVoto;
 }
