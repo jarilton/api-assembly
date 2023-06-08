b473f72336bfde483b65d79377084d9b2b9ea184
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 20:23:45 -0300

    feat: Configuring Swagger (OPEN API) in the API

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('NestJS API description')
+    .setVersion('1.0')
+    .setContact('NestJS', 'https://nestjs.com/', 'nestjs@nestjs')
+    .addTag('nestjs')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
