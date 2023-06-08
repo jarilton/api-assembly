ae86f0229108357bd94de01fe63fb402ef50b4a4
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 17:22:25 -0300

    feat: Creating the Vote Registration Resource

--- src/votos/votos.controller.ts
+++ src/votos/votos.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import { VotosService } from './votos.service';
+import { PautasService } from 'src/pautas/pautas.service';
+import { RegistroVotoResource } from './votos.resource';
+import { Response } from 'express';
 
-@Controller('votos')
-export class VotosController {}
+@Controller('pautas/:id/votos')
+export class VotosController {
+  constructor(
+    private readonly votosService: VotosService,
+    private readonly pautasService: PautasService,
+  ) {}
+
+  @Post()
+  async RegistrarVoto(
+    @Param('id') idPauta: string,
+    @Body() registroVotoResource: RegistroVotoResource,
+    @Res() response: Response,
+  ) {
+    return response.status(HttpStatus.ACCEPTED).send();
+  }
+}
---
+++ src/votos/votos.resource.ts
@@ -0,0 +1,6 @@
+import { OpcaoVoto } from './votos.entity';
+
+export class RegistroVotoResource {
+  cpf: string;
+  opcaoVoto: OpcaoVoto;
+}
