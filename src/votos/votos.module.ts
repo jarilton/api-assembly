48e04e7a57a36e41c6de27448e81688c408b92e4
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 17:32:31 -0300

    feat: Importing components from other modules

--- src/votos/votos.module.ts
+++ src/votos/votos.module.ts
@@ -4,10 +4,11 @@ import { DatabaseModule } from 'src/database/database.module';
 import { votosProviders } from './votos.providers';
 import { VotosService } from './votos.service';
 import { AssociadosService } from './associados/associados.service';
+import { PautasModule } from 'src/pautas/pautas.module';
 
 @Module({
   controllers: [VotosController],
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PautasModule],
   providers: [...votosProviders, VotosService, AssociadosService],
 })
 export class VotosModule {}
