65a6904e5790eb668a4cbb72e2b5eb4a0fa0330d
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-06-08 17:40:45 -0300

    feat: member registration and search logic

--- src/votos/associados/associados.service.ts
+++ src/votos/associados/associados.service.ts
@@ -8,4 +8,27 @@ export class AssociadosService {
     @Inject('ASSOCIADOS_REPOSITORY')
     private readonly associadoRepository: Repository<Associados>,
   ) {}
+
+  async obterPorCpf(cpf: string): Promise<Associados> {
+    return await this.associadoRepository.findOne({
+      where: {
+        cpf: cpf,
+      },
+    });
+  }
+
+  async recuperarOuCadastrar(cpf: string): Promise<Associados> {
+    const associado: Associados = await this.obterPorCpf(cpf);
+
+    if (associado) {
+      return associado;
+    }
+
+    const novoAssociado = new Associados();
+    novoAssociado.cpf = cpf;
+
+    await this.associadoRepository.save(novoAssociado);
+
+    return novoAssociado;
+  }
 }
